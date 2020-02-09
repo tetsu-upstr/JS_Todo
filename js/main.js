f12f754e2391831ed117201cf4154a7ef1b2662e
tetsu-upstr <bodomu@gmail.com>
2020-02-10 04:12:22 +0900

    チェックボックス生成

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
-// ボタンのIDを取得
+// HTML読み込み後にイベントリスナーを発火させる
+window.onload = function () {
+  
+  // ボタンのIDを取得
   var btn = document.getElementById('btn');
-  console.log(btn);
 
   // ボタンをクリックした時の処理
   btn.addEventListener('click', function() {
 
-    // todoタスクを取得
-    var task = document.getElementById('text-msg').value;
-    console.log(task);
+  // todoタスクを取得
+  var task = document.getElementById('text-msg').value;
 
-    // liリストを生成
-    var taskList = document.createElement('li');
-    taskList.innerHTML = task;
-    
-    // ulリスト内にタスクを表示
-    var taskUl = document.getElementById('todo-list');
-    taskUl.appendChild(taskList);
+  // liリストを生成
+  var taskList = document.createElement('li');
 
-    
+    taskList.innerHTML =
+     '<input class="check-btn" type="checkbox" name="taskCheck">' + task;
+     document.getElementById('text-msg').value = '';
+     document.getElementById('text-msg').focus();
+  
+  
+  // ulリスト内にタスクを表示
+  var taskUl = document.getElementById('todo-list');
+  taskUl.appendChild(taskList);
 
   });
+
+
+  // チェックボックスをチェックしたら完了にする
+  var check = document.querySelector('.check-btn');
+
+  check.addEventListener('change', (event) => {
+    var value = event.target.checked;
+    console.log(value);
+  });
+};
+
+
+
+  
+
+
+
+  
+
+ 
+
 
 
