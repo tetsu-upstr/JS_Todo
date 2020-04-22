a2f7c50764e1c7d6a929424f2eed5d37a242667c
tetsu-upstr <bodomu@gmail.com>
2020-04-23 05:43:22 +0900

    スペース削除

--- js/main.js
+++ js/main.js
@@ -1,49 +1,31 @@
-// HTML読み込み後にイベントリスナーを発火させる
-window.onload = function () {
-  
-  // ボタンのIDを取得
-  var btn = document.getElementById('btn');
+// ボタンのIDを取得
+var btn = document.getElementById('btn');
 
-  // ボタンをクリックした時の処理
-  btn.addEventListener('click', function() {
+// ボタンをクリックした時の処理
+btn.addEventListener('click', function() {
 
-  // todoタスクを取得
-  var task = document.getElementById('text-msg').value;
+// todoタスクを取得
+var task = document.getElementById('text-msg').value;
 
+if (task != '') {
   // liリストを生成
   var taskList = document.createElement('li');
 
-    taskList.innerHTML =
-     '<input class="check-btn" type="checkbox" name="taskCheck">' + task;
-     document.getElementById('text-msg').value = '';
-     document.getElementById('text-msg').focus();
-  
+  taskList.innerHTML = '<input class="check-btn" type="checkbox" name="taskCheck">' + task;
+  document.getElementById('text-msg').value = '';
+  document.getElementById('text-msg').focus();
   
   // ulリスト内にタスクを表示
   var taskUl = document.getElementById('todo-list');
   taskUl.appendChild(taskList);
+}
 
-  });
-
-
-  // チェックボックスをチェックしたら完了にする
-  var check = document.querySelector('.check-btn');
-
-  check.addEventListener('change', (event) => {
-    var value = event.target.checked;
-    console.log(value);
-  });
-};
+});
 
 
-
+// チェックボックスをチェックしたら完了にする
+var check = document.getElementsByClassName('check-btn');
+  console.log(check);
+if (check.value == "checked" ) {
   
-
-
-
-  
-
- 
-
-
-
+}
