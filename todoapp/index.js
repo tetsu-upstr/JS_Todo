bad99ed08a52b52eeb2698395e29494c880c68cc
tetsu-upstr <bodomu@gmail.com>
2020-04-24 06:37:24 +0900

    templete

--- todoapp/index.js
+++ todoapp/index.js
@@ -1,4 +1,6 @@
 import { App } from "./src/App.js";
 const app = new App();
 
+app.mount();
+
 // console.log("index.js: loaded");
--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,6 +1,18 @@
-console.log("App.js: loaded");
+// elementタグ関数をimport
+import { element } from "./view/html-util.js";
+
 export class App {
-  constructor(){
-    console.log("App initialized");
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    const containerElement = document.querySelector("#js-todo-list");
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+    
+
+    formElement.addEventListener("submit", (event) => {
+      // submitの本来の動作を止める（URLに送信されリロードするformの動き）
+      event.preventDefault();
+      console.log(`入力欄の値： ${inputElement.value}`);
+    });
   }
 }
